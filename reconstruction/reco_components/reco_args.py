a9a8b990aedec35760332546703afd67af5ca7a2
Samuel Ferraro <113254671+samf25@users.noreply.github.com>
2025-07-21 15:43:43 -0400

    Container paths changed

--- reconstruction/reco_components/reco_args.py
+++ reconstruction/reco_components/reco_args.py
@@ -17,14 +17,14 @@ def get_reco_args():
         "--MatFile",
         help="Material maps file for tracking",
         type=str,
-        default="/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/actstracking-1.3.1-zxsyo4d3u3cfzn55k4yyigif7jsyzshc/share/ACTSTracking/data/material-maps.json",
+        default="/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/actstracking-1.3.1-kunm7cmwx7eytrs7dath3cx3egcm7ifd/share/ACTSTracking/data/material-maps.json",
     )
 
     parser.add_argument(
         "--TGeoFile",
         help="TGeometry file for tracking",
         type=str,
-        default="/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/actstracking-1.3.1-zxsyo4d3u3cfzn55k4yyigif7jsyzshc/share/ACTSTracking/data/MuColl_v1.root",
+        default="/opt/spack/opt/spack/linux-almalinux9-x86_64/gcc-11.5.0/actstracking-1.3.1-kunm7cmwx7eytrs7dath3cx3egcm7ifd/share/ACTSTracking/data/MuColl_v1.root",
     )
 
     parser.add_argument(
